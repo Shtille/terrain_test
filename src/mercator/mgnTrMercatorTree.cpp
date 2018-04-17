475c7ef97eea739dc892d9b0555ff91305de6946
Vladimir <v.shtille@gmail.com>
2018-04-17 16:56:10 +0300

    optimized look via reducing texture detail required

--- src/mercator/mgnTrMercatorTree.cpp
+++ src/mercator/mgnTrMercatorTree.cpp
@@ -19,7 +19,7 @@
 namespace {
     // The more detail coefficient is, the less detalization is required
     const float kGeoDetail = 6.0f;
-    const float kTexDetail = 3.0f;
+    const float kTexDetail = 1.0f;
 }
 
 namespace mgn {
